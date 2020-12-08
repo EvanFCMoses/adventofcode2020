acd8c7fe1900fd2c0f6c09ff8e166661ed9f4994
evan moses <evanfcmoses@gmail.com>
2020-12-07 22:02:03 -0500

    partially finished day 6

---
+++ day6/questionaire.py
@@ -0,0 +1,66 @@
+
+
+class QuestionaireResults:
+	def __init__(self):
+		# self.groupOfRows = self.parseForRows('input.txt')
+		self.listOfGroupResults = self.parseRows(self.parseForRows('input.txt'))
+
+
+	def parseForRows(self, fileName):
+		file = open(fileName, 'r')
+		array = []
+		currentRows = []
+		for row in file:
+			row = row[:-1]
+			if row == "":
+				array.append(currentRows)
+				currentRows = []
+			else:
+				currentRows.append(row)
+
+		array.append(currentRows)
+		return array
+
+	def parseRows(self, rowOfRows):
+		
+		groupResponses = []
+		for rows in rowOfRows:
+			response = GroupAnswers()
+			response.groupSize = len(rows)
+			for row in rows:
+				self.appendAnswersToDict(response.answerDict, row) 
+			groupResponses.append(response)
+		return groupResponses
+
+
+	def appendAnswersToDict(self, answers, row):
+		for char in row:
+			if char in answers:
+				answers[char] = answers[char] + 1
+			else:
+				answers[char] = 1
+
+	def getDictWithUnanimousAnswers(self):
+		print("todo")
+
+
+class GroupAnswers:
+	def __init__(self):
+
+		self.answerDict = {}
+		self.groupSize = 0
+
+
+questionaire = QuestionaireResults()
+
+addition = 0
+for result in questionaire.listOfGroupResults:
+	print(len(result.answerDict.keys()))
+	print(result.answerDict)
+	print(result.groupSize)
+	addition = addition + len(result.answerDict.keys())
+
+
+for row in questionaire.listOfGroupResults:
+	print(row.answerDict)
+print(addition)
